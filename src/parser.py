b5693c12f3c682dc96352d288a25d8b0470816bc
Nicolas Compère <75181320+ncompere@users.noreply.github.com>
2023-02-02 00:01:27 +0100

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,2 +1,18 @@
 import numpy as np
 
+
+def read_data(file):
+    edges = []
+    with open(file, 'r') as file:
+        lines = file.readlines()
+        first_line = lines[1].split()
+        for line in lines[2:]:
+            edge = line.split()
+            arrete = (int(edge[0]), int(edge[1]))
+            edges = np.append(edges, arrete)
+        return int(first_line[0]), int(first_line[1]), edges
+
+
+nb_vertex, nb_edges, edges = read_data(file='HB113-20230131/494_bus.mtx.rnd')
+
+print(edges)
