18a4708dcf9d3a7ac4659da5b3b1ccb7cc571e0e
Nicolas Compère <75181320+ncompere@users.noreply.github.com>
2023-02-02 14:10:03 +0100

    Finished parser
    Discuss on the data structure to use and complexity of the parser function

--- src/parser.py
+++ src/parser.py
@@ -6,13 +6,17 @@ def read_data(file):
     with open(file, 'r') as file:
         lines = file.readlines()
         first_line = lines[1].split()
+        i = 0
         for line in lines[2:]:
             edge = line.split()
             arrete = (int(edge[0]), int(edge[1]))
-            edges = np.append(edges, arrete)
-        return int(first_line[0]), int(first_line[1]), edges
+            edges.insert(i, arrete)
+            i += 1
+        # return int(first_line[0]), int(first_line[1]), edges
+        return edges
 
 
-nb_vertex, nb_edges, edges = read_data(file='HB113-20230131/494_bus.mtx.rnd')
+# nb_vertex, nb_edges, edges = read_data(file='HB113-20230131/494_bus.mtx.rnd')
+edges = read_data(file='HB113-20230131/494_bus.mtx.rnd')
 
 print(edges)
